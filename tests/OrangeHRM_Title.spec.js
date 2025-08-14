42afff0c1fc0a005fe7d0e70ebbfde3d17261ac1
EmreAnliacik <emreanliacik@hotmail.com>
2025-08-14 13:07:09 +0200

    Initial commit

---
+++ tests/OrangeHRM_Title.spec.js
@@ -0,0 +1,8 @@
+// @ts-check
+
+import { test, expect } from '@playwright/test';
+test('Validate OrangeHRM Website title', async({page}) => {
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/');
+    await expect(page).toHaveTitle(/OrangeHRM/);
+})
